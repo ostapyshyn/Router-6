70d75fa86145ebe5901b0de3fab7e5c618c83976
Volodymyr Ostapyshyn <ostapyshyn@gmail.com>
2022-04-15 17:21:51 +0300

    Add message

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,11 +3,16 @@ import { LogContext } from '../LogContext';
 // import '../index.css';
 
 const Home = () => {
-  const { setIsLogged } = useContext(LogContext);
+  const { isLogged, setIsLogged } = useContext(LogContext);
 
   return (
     <>
       <h1>Home page</h1>
+      {isLogged ? (
+        <h2>You're logged in and have access to posts and about pages</h2>
+      ) : (
+        <h2>You're logged out and don't have access to posts and about pages</h2>
+      )}
       <div className='center'>
         <button className='button' onClick={() => setIsLogged(true)}>
           LogIn
--- src/pages/Notfound.jsx
+++ src/pages/Notfound.jsx
@@ -3,7 +3,10 @@ import { Link } from 'react-router-dom';
 const Notfound = () => {
   return (
     <div>
-      This page doesn't exist. Go to <Link to='/'>home.</Link>
+      This page doesn't exist. Go to{' '}
+      <Link to='/'>
+        <span className='home'>home</span>.
+      </Link>
     </div>
   );
 };
